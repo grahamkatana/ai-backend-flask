5e005d373a5ef52ba2dccc389ebaef04254fba05
grahamkatana <graykatanakenny@gmail.com>
2025-03-01 12:59:38 +0200

    Agent model

---
+++ app/models/Agents.py
@@ -0,0 +1,20 @@
+from app.config.extensions import db
+from datetime import datetime
+import uuid
+
+
+class Agent(db.Model):
+    __tablename__ = "agents"
+
+    id = db.Column(db.Text(), primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255), nullable=True)
+    online = db.Column(db.Boolean, default=False)
+    attributes = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime,
+        nullable=True,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
